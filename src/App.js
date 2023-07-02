32d5ebd06d91bf854f4d8ca51210373ec440b09d
Bodhaansh Ravipati <45471123+BodhaanshRavipati325@users.noreply.github.com>
2023-07-02 12:17:06 -0500

    Add styling on Navbar

--- src/App.js
+++ src/App.js
@@ -3,16 +3,15 @@ import * as React from "react";
 import "/Users/bodhaanshravipati/Documents/create-react-app/my-app/src/styles/Navbar.css";
 
 import Projects from "/Users/bodhaanshravipati/Documents/create-react-app/my-app/src/components/Projects.js";
-import Slider from "/Users/bodhaanshravipati/Documents/create-react-app/my-app/src/components/Slider.js";
 
 import { Parallax, ParallaxLayer } from "@react-spring/parallax";
-import { TypeAnimation } from "react-type-animation";
 
 import github from "/Users/bodhaanshravipati/Documents/create-react-app/my-app/src/images/github.png";
 import instagram from "/Users/bodhaanshravipati/Documents/create-react-app/my-app/src/images/instagram.png";
 import linkedin from "/Users/bodhaanshravipati/Documents/create-react-app/my-app/src/images/linkedin.png";
+import signature from "/Users/bodhaanshravipati/Documents/create-react-app/my-app/src/images/header.png";
 
-import styles from "/Users/bodhaanshravipati/Documents/create-react-app/my-app/src/styles/styles.module.css";
+import FrontPage from "./components/FrontPage";
 
 function scrollDown() {
   window.scroll({ top: 1, left: 0, behavior: "smooth" });
@@ -24,7 +23,13 @@ export function App() {
       <Parallax pages={2}>
         <ParallaxLayer id="github" sticky={{ start: 0.005, end: 0.005 }}>
           <a href="https://github.com/BodhaanshRavipati325" target="_blank">
-            <img src={github}></img>
+            <img width="48px" height="48px" src={github}></img>
+            <img
+              id="signature"
+              width="206px"
+              height="96px"
+              src={signature}
+            ></img>
           </a>
         </ParallaxLayer>
 
@@ -33,7 +38,7 @@ export function App() {
             href="https://www.instagram.com/bodhiishere/?hl=en"
             target="_blank"
           >
-            <img src={instagram}></img>
+            <img width="48px" height="48px" src={instagram}></img>
           </a>
         </ParallaxLayer>
 
@@ -42,42 +47,10 @@ export function App() {
             href="https://www.linkedin.com/in/bodhaansh-ravipati-16515419b/"
             target="_blank"
           >
-            <img src={linkedin}></img>
+            <img width="48px" height="48px" src={linkedin}></img>
           </a>
         </ParallaxLayer>
-
-        <ParallaxLayer
-          id="layerA"
-          speed={4}
-          // factor={1.5}
-          style={{
-            backgroundSize: "cover",
-          }}
-        >
-          <ParallaxLayer sticky={{ start: 0.25, end: 0.25 }} speed={1}>
-            <h1 id="name">
-              <TypeAnimation
-                sequence={[
-                  // Same substring at the start will only be typed out once, initially
-                  "Bodhaansh Ravipati",
-                  1000, // wait 1s before replacing "Mice" with "Hamsters"
-                ]}
-                wrapper="span"
-                speed={25}
-                repeat={Infinity}
-              />
-            </h1>
-          </ParallaxLayer>
-          <ParallaxLayer sticky={{ start: 0.45, end: 0.45 }} speed={0.5}>
-            <h2 id="job">Full Stack Developer</h2>
-          </ParallaxLayer>
-          <ParallaxLayer sticky={{ start: 0.25, end: 0.25 }} speed={1}>
-            <div className={styles.container}>
-              <Slider>SSHHH!!</Slider>
-            </div>
-          </ParallaxLayer>
-        </ParallaxLayer>
-
+        <FrontPage></FrontPage>
         <Projects></Projects>
       </Parallax>
     </>
--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -1,9 +1,12 @@
-import React from "react";
+import * as React from "react";
+
 import "/Users/bodhaanshravipati/Documents/create-react-app/my-app/src/styles/Navbar.css";
 
 import Slider from "/Users/bodhaanshravipati/Documents/create-react-app/my-app/src/components/Slider.js";
 
 import { ParallaxLayer } from "@react-spring/parallax";
+import { TypeAnimation } from "react-type-animation";
+
 
 import styles from "/Users/bodhaanshravipati/Documents/create-react-app/my-app/src/styles/styles.module.css";
 
@@ -11,19 +14,36 @@ export default function FrontPage() {
   return (
     <>
       <ParallaxLayer
-        id="layerA"
-        speed={1}
-        // factor={1.5}
-        style={{
-          backgroundSize: "cover",
-        }}
-      ></ParallaxLayer>
-      <ParallaxLayer sticky={{ start: 0.25, end: 0.25 }} speed={1}>
-        <h1 id="name">Bodhaansh Ravipati</h1>
-      </ParallaxLayer>
-      <ParallaxLayer sticky={{ start: 0.45, end: 0.45 }} speed={1}>
-        <h2 id="job">Full Stack Developer</h2>
-      </ParallaxLayer>
+          id="layerA"
+          speed={4}
+          // factor={1.5}
+          style={{
+            backgroundSize: "cover",
+          }}
+        >
+          <ParallaxLayer sticky={{ start: 0.25, end: 0.25 }} speed={1}>
+            <h1 id="name">
+              <TypeAnimation
+                sequence={[
+                  // Same substring at the start will only be typed out once, initially
+                  "Bodhaansh Ravipati",
+                  1000, // wait 1s before replacing "Mice" with "Hamsters"
+                ]}
+                wrapper="span"
+                speed={25}
+                repeat={Infinity}
+              />
+            </h1>
+          </ParallaxLayer>
+          <ParallaxLayer sticky={{ start: 0.45, end: 0.45 }} speed={0.5}>
+            <h2 id="job">Full Stack Developer</h2>
+          </ParallaxLayer>
+          <ParallaxLayer sticky={{ start: 0.25, end: 0.25 }} speed={1}>
+            <div className={styles.container}>
+              <Slider>SSHHH!!</Slider>
+            </div>
+          </ParallaxLayer>
+        </ParallaxLayer>
     </>
   );
 }
